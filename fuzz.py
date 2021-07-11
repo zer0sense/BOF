2afa832f98e46fc26d743f47c14b06dba11eb85d
zer0sense <kalifornia909@gmail.com>
2021-07-11 13:46:29 +0900

    fuzz

--- fuzz.py
+++ fuzz.py
@@ -5,11 +5,12 @@ from time import sleep
 ip = ''
 port= 
 buffer = "A" * 100 
+command = "TRUN /.:/"
 
 
 while True: 
     try: 
-        payload = "TRUN /.:/" + buffer 
+        payload = command + buffer 
         
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         s.connect((ip,port)) 
